73e0288f8e7ba11c0414d81d558488c76bea8824
deewondev <deewondev@gmail.com>
2023-05-22 20:16:10 +0100

    update permutation

--- permutation.js
+++ permutation.js
@@ -1,6 +1,6 @@
 import { myFactorial } from "./factorial.js";
 
-function myPermutation(n, r) {
+const myPermutation = (n, r) => {
     let result;
     let denum = n - r;
     result = myFactorial(n) / myFactorial(denum);
