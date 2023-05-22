6438496ce81bb14dc2982658095fc06817c88970
deewondev <deewondev@gmail.com>
2023-05-22 10:42:52 +0100

    create combination

--- combination.js
+++ combination.js
@@ -1,1 +1,12 @@
+import { myFactorial } from "./factorial.js";
 
+function myCombination(n, r) {
+    let num, denum, result;
+    let x = n - r;
+    num = myFactorial(n);
+    denum = myFactorial(r) * myFactorial(x);
+    result = num / denum;
+    return result;
+}
+
+export { myCombination };
